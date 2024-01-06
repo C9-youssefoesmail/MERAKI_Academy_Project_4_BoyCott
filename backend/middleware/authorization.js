c3d05b8da520118be02187b217e29ea85f46e5f2
youssefoesmail <youssefo.esmail@gmail.com>
2024-01-06 17:27:13 +0300

    create authorization

---
+++ backend/middleware/authorization.js
@@ -0,0 +1,16 @@
+
+
+const authorization = (text) => {
+    return(req,res,next) => {
+        const {permissions} = req.token.role
+        if(!permissions.includes(text)){
+            res.status(403).json({
+                success: false,
+                message: "Unauthorized"
+            })
+        }
+        else{
+            next()
+        }
+    }
+}

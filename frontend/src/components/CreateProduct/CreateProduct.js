6369c5e769848cc60e7ad572171a59d1403a02bd
youssefoesmail <youssefo.esmail@gmail.com>
2024-01-14 23:24:03 +0300

    finish createProduct & modify Login layout

--- frontend/src/components/CreateProduct/CreateProduct.js
+++ frontend/src/components/CreateProduct/CreateProduct.js
@@ -13,7 +13,8 @@ import {
   styled,
 } from "@mui/material";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { LoginContext } from "../../App";
 
 //styled
 const Item = styled(Paper)(({ theme }) => ({
@@ -28,10 +29,20 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const CreateProduct = () => {
+  
+  //useContext
+  const { token, userId } = useContext(LoginContext);
+
   //uaeState
-  const [boycott, setBoycott] = useState(false)
-  const [categoryId, setCategoryId] = useState("");
   const [allCategories, setAllCategories] = useState([]);
+  const [createdBy, setCreatedBy] = useState(userId)
+  const [isSafeProduct, setIsSafeProduct] = useState(true)
+  const [categories, setCategories] = useState("");
+  const [productImage,setProductImage] = useState("")
+  const [productName,setProductName] = useState("")
+  const [reason, setReason] = useState("")
+  const [oppositeProduct, setOppositeProduct] = useState("")
+  const [link, setLink] = useState("")
 
   //goToProduct
   const getCategories = () => {
@@ -45,10 +56,32 @@ const CreateProduct = () => {
       });
   };
 
+  //createNewProduct
+  const createNewProduct = () => {
+    if(createdBy, isSafeProduct !== null, categories, productImage, productName, reason, link){
+      axios
+        .post(`http://localhost:5000/products`, newProduct, {
+          headers: { authorization: `Bearer ${token}` },
+        })
+        .then((result) => {
+          console.log(result.data);
+        })
+        .catch((err) => {
+          console.log("err: ", err);
+        });
+    }
+    else
+    {
+      console.log("something is missed");
+    }
+  }
+
   //useEffect
   useEffect(() => {
     getCategories();
   }, []);
+
+  const newProduct = {createdBy, isSafeProduct, categories, productImage, productName, reason, link}
 
   return (
     <div>
@@ -56,9 +89,14 @@ const CreateProduct = () => {
         <Grid container spacing={0}>
           <Grid item xs={4}>
             <Item>
-              <Button sx={{ height: 400 }} required>
-                add Picture
-              </Button>
+            <TextField
+                id="outlined-multiline-static"
+                label="add the URL picture"
+                multiline
+                rows={4}
+                required
+                onChange={(e)=>{setProductImage(e.target.value)}}
+              />
             </Item>
             <Item>
               <FormControlLabel
@@ -67,7 +105,7 @@ const CreateProduct = () => {
                   <Checkbox
                     onChange={(e) => {
                       console.log(!e.target.checked);
-                      setBoycott(!e.target.checked);
+                      setIsSafeProduct(!e.target.checked);
                     }}
                   />
                 }
@@ -81,7 +119,7 @@ const CreateProduct = () => {
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
                   label="category"
-                  onChange={(e)=>{setCategoryId(e.target.value)}}
+                  onChange={(e)=>{setCategories(e.target.value)}}
                 >
                   {allCategories.map((type, i) => {
                     return <MenuItem value={type._id}>{type.typeName}</MenuItem>;
@@ -89,6 +127,14 @@ const CreateProduct = () => {
                 </Select>
               </FormControl>
             </Item>
+            <Button
+                onClick={() => {
+                  createNewProduct()
+                  console.log(" createdBy =>",createdBy," isSafeProduct =>", isSafeProduct," categories =>",categories , "productImage =>", productImage , " productName =>" , productName , " reason =>", reason, " oppositeProduct =>", oppositeProduct, " link =>",link)
+                }}
+              >
+                Add
+              </Button>
           </Grid>
           <Grid item xs={8}>
             <Item>
@@ -97,6 +143,7 @@ const CreateProduct = () => {
                 label="product name"
                 multiline
                 required
+                onChange={(e)=>{setProductName(e.target.value)}}
               />
             </Item>
             <Item>
@@ -106,6 +153,7 @@ const CreateProduct = () => {
                 multiline
                 rows={4}
                 required
+                onChange={(e)=>{setReason(e.target.value)}}
               />
             </Item>
             <Item>
@@ -115,6 +163,7 @@ const CreateProduct = () => {
                 multiline
                 rows={4}
                 required
+                onChange={(e)=>{setLink(e.target.value)}}
               />
             </Item>
           </Grid>
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -3,12 +3,26 @@ import React, { useContext, useState } from "react";
 import { LoginContext } from "../../App";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
+import { FilledInput, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 //the Login Function
 const Login = () => {
-  
+
+  //password state
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  //handleClickShowPassword
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  //handleMouseDownPassword
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   //useState
-  const { token, setToken, setIsLoggedIn, setUserStatus, setUserId } = useContext(LoginContext);
+  const { token, setToken, setIsLoggedIn, setUserStatus, setUserId } =
+    useContext(LoginContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
@@ -24,14 +38,14 @@ const Login = () => {
           console.log(result.data.token);
           console.log(result.data._role.role);
           console.log(result.data._userId);
-          setUserId(result.data._userId)
-          setUserStatus(result.data._role.role)
+          setUserId(result.data._userId);
+          setUserStatus(result.data._role.role);
           setToken(result.data.token);
           setIsLoggedIn(true);
           localStorage.setItem("token", result.data.token);
           localStorage.setItem("isLoggedIn", true);
           localStorage.setItem("userStatus", result.data._role.role);
-          localStorage.setItem("userId", result.data._userId)
+          localStorage.setItem("userId", result.data._userId);
           navigate("/");
         })
         .catch((err) => {
@@ -46,20 +60,32 @@ const Login = () => {
   return (
     <div className="Login">
       <p>Login:</p>
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => {
+      <TextField id="outlined-textarea" label="email" multiline required onChange={(e) => {
           setEmail(e.target.value);
-        }}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
+        }}/>
+        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password" >Password</InputLabel>
+          <OutlinedInput
+          onChange={(e)=>{
+            setPassword(e.target.value);
+          }}
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
       <button
         className="button"
         onClick={() => {

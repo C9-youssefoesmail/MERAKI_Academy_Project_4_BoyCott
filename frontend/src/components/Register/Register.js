2022d4666d6ad1ff11ad917c8d4e088dfafa27e6
youssefoesmail <youssefo.esmail@gmail.com>
2024-01-14 23:31:22 +0300

    modify Register

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,10 +1,24 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 //Register
 const Register = () => {
+  
+  //password state
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  //handleClickShowPassword
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  //handleMouseDownPassword
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   //useState
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
@@ -42,27 +56,35 @@ const Register = () => {
     <>
       <div className="Register">
         <p>Register:</p>
-        <input
-          type="text"
-          placeholder="First Name"
-          onChange={(e) => {
-            setUserName(e.target.value);
-          }}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => {
+        <TextField id="outlined-textarea" label="user name" multiline required onChange={(e) => {
+          setUserName(e.target.value);
+        }}/>
+        <TextField id="outlined-textarea" label="email" multiline required onChange={(e) => {
+          setEmail(e.target.value);
+        }}/>
+        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password" >Password</InputLabel>
+          <OutlinedInput
+          onChange={(e)=>{
             setPassword(e.target.value);
           }}
-        />
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
         <button
           className="button"
           onClick={() => {

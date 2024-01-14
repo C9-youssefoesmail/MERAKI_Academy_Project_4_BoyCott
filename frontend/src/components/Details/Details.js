ce9ee6f7681dce951ef06e1e12a2b51d695360c5
youssefoesmail <youssefo.esmail@gmail.com>
2024-01-14 13:45:44 +0300

    createComment with commenter

--- frontend/src/components/Details/Details.js
+++ frontend/src/components/Details/Details.js
@@ -31,13 +31,13 @@ const Item = styled(Paper)(({ theme }) => ({
 
 //Details function
 const Details = () => {
-
   //useContext
   const { token } = useContext(LoginContext);
 
   //useState
   const [productDetails, setProductDetails] = useState({});
-  const [comm, setComm] = useState("")
+  const [comment, setComment] = useState("");
+  const [product, setProduct] = useState("");
 
   //product id from URL
   const { id } = useParams();
@@ -48,6 +48,7 @@ const Details = () => {
     axios
       .get(`http://localhost:5000/products/search_1/${id}`)
       .then((result) => {
+        setProduct(id);
         setProductDetails(result.data._product[0]);
       })
       .catch((err) => {
@@ -58,26 +59,29 @@ const Details = () => {
   //createComment
   const createComment = (id) => {
     console.log(id);
-    if(comm){
+    if (comment && product) {
       axios
-      .post(`http://localhost:5000/comments`, comm, {
-        headers: { authorization: `Bearer ${token}` },
-      })
-      .then((result)=>{
-        goToProduct()
-      })
-      .catch((err)=>{
-        console.log("error => ", err);
-      })
+        .post(`http://localhost:5000/comments`, commentVar, {
+          headers: { authorization: `Bearer ${token}` },
+        })
+        .then((result) => {
+          //! How to filter
+          goToProduct();
+        })
+        .catch((err) => {
+          console.log("error => ", err);
+        });
+    } else {
+      console.log("out of IF", comment, product);
     }
-  }
+  };
 
   //useEffect
   useEffect(() => {
     goToProduct();
   }, []);
 
-  const fdjj = {comm , }
+  const commentVar = { comment, product };
 
   //return of the component
   return (
@@ -111,25 +115,49 @@ const Details = () => {
             <Item>
               {productDetails.categories && productDetails.categories.typeName}
             </Item>
+            <Item>
+              {productDetails.oppositeProduct
+                ? productDetails.oppositeProduct.productName
+                : "Add opposite"}
+            </Item>
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={10}>
             <Item>
               {productDetails.review &&
                 productDetails.review.map((comment, i) => {
                   console.log(comment);
-                  return <p>{comment.comment}</p>;
+                  return (
+                    <>
+                      <p>{comment.createdBy} comment : {comment.comment}</p>
+                    </>
+                  );
                 })}
               <input
                 type="text"
                 placeholder="comment..."
                 onChange={(e) => {
-                  setComm(e.target.value)
-                  console.log(comm);
+                  setComment(e.target.value);
+                  console.log(comment);
                 }}
               />
-              <Button onClick={() => {
-                createComment(productDetails._id)
-              }}>Add Comment</Button>
+              <Button
+                onClick={() => {
+                  createComment(productDetails._id);
+                }}
+              >
+                Add Comment
+              </Button>
+            </Item>
+          </Grid>
+          <Grid item xs={2}>
+            <Item>
+              <Button
+                onClick={() => {
+                  createComment(productDetails._id);
+                }}
+              >
+                Add Comment
+              </Button>
             </Item>
           </Grid>
         </Grid>

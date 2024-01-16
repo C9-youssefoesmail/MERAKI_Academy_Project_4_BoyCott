da800ebb0e6ae6f0211b0e6526edbfed3877cab3
youssefoesmail <youssefo.esmail@gmail.com>
2024-01-16 21:28:12 +0300

    modify Nav.js

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -1,24 +1,19 @@
 import React, { useContext, useState } from "react";
 import {
   AppBar,
-  Avatar,
-  Badge,
   Box,
   Button,
-  InputBase,
+  ButtonGroup,
   ListItem,
   ListItemButton,
   ListItemIcon,
-  Menu,
-  MenuItem,
   Switch,
   Toolbar,
   Typography,
-  alpha,
   styled,
 } from "@mui/material";
 import { useNavigate, Link } from "react-router-dom";
-import { Mail, ModeNight, Notifications } from "@mui/icons-material";
+import { ModeNight } from "@mui/icons-material";
 import { LoginContext } from "../../App";
 import "./style.css";
 
@@ -28,13 +23,6 @@ const StyledToolbar = styled(Toolbar)({
   justifyContent: "space-between",
 });
 
-const Search = styled("div")(({ theme }) => ({
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  marginLeft: 0,
-  width: "20%",
-}));
-
 const Icons = styled(Box)(({ theme }) => ({
   display: "flex",
   gap: "15px",
@@ -43,7 +31,8 @@ const Icons = styled(Box)(({ theme }) => ({
 
 const Nav = () => {
   //!---------------useContext
-  const { isLoggedIn, setToken, setIsLoggedIn, setUserStatus, userStatus } = useContext(LoginContext);
+  const { isLoggedIn, setToken, setIsLoggedIn, setUserStatus, userStatus } =
+    useContext(LoginContext);
 
   //!---------------useState
   const [open, setOpen] = useState(false);
@@ -56,89 +45,49 @@ const Nav = () => {
         <StyledToolbar>
           <Typography>
             <Link to="/">
-              <Button variant="contained">
-                Home
-              </Button>
+              <Button variant="contained">Home</Button>
             </Link>
-            <ListItem disablePadding>
-              <ListItemButton
-                component="a"
-                href=""
-                sx={{
-                  backgroundColor: "#aaa",
-                  color: "#aaa",
-                  opacity: "0.5",
-                }}
-              >
-                <ListItemIcon>
-                  <ModeNight />
-                </ListItemIcon>
-                <Switch />
-              </ListItemButton>
-            </ListItem>
           </Typography>
-
-          <Search>
-            <InputBase placeholder="search" />
-          </Search>
           {isLoggedIn ? (
             <Icons>
-              <Badge badgeContent={4}>
-                <Mail />
-              </Badge>
-              <Badge badgeContent={2}>
-                <Notifications />
-              </Badge>
-              <Avatar
-                sx={{ width: 30, height: 30 }}
-                src=""
-                onClick={(e) => setOpen(true)}
-              />
+              <ButtonGroup variant="contained">
+                <Link to="/Profile">
+                  <Button>Profile</Button>
+                </Link>
+                {userStatus === "admin" ? (
+                  <Link to="/CreateProduct">
+                    <Button>Create Product</Button>
+                  </Link>
+                ) : null}
+                <Link
+                  to="/"
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    localStorage.removeItem("isLoggedIn");
+                    localStorage.removeItem("userStatus");
+                    localStorage.removeItem("userId");
+                    setToken("");
+                    setIsLoggedIn(false);
+                    setUserStatus("");
+                  }}
+                >
+                  <Button>Logout</Button>
+                </Link>
+              </ButtonGroup>
             </Icons>
           ) : (
             <Icons>
-              <Link to="/Login">Login</Link>
-              <Link to="/Register">Register</Link>
+              <ButtonGroup variant="contained">
+                <Link to="/Login">
+                  <Button>Login</Button>
+                </Link>
+                <Link to="/Register">
+                  <Button>Register</Button>
+                </Link>
+              </ButtonGroup>
             </Icons>
           )}
         </StyledToolbar>
-        <Menu
-          id="demo-positioned-menu"
-          aria-labelledby="demo-positioned-button"
-          open={open}
-          onClick={(e) => setOpen(false)}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-        >
-          <MenuItem>
-            <Link to="/Profile">Profile</Link>
-          </MenuItem>
-          {userStatus === "admin" ? <MenuItem>
-            <Link to="/CreateProduct">Create Product</Link>
-          </MenuItem> : ""}
-          <MenuItem>
-            <Link
-              to="/"
-              onClick={() => {
-                localStorage.removeItem("token");
-                localStorage.removeItem("isLoggedIn");
-                localStorage.removeItem("userStatus");
-                localStorage.removeItem("userId")
-                setToken("");
-                setIsLoggedIn(false);
-                setUserStatus("")
-              }}
-            >
-              Logout
-            </Link>
-          </MenuItem>
-        </Menu>
       </AppBar>
     </>
   );

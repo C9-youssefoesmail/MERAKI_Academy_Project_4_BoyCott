7fa7463c09f322390b172e54ee09ae2aaa124bab
youssefoesmail <youssefo.esmail@gmail.com>
2024-04-05 17:59:36 +0300

    deploy

--- backend/models/db.js
+++ backend/models/db.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/project_4")
+  .connect("mongodb+srv://youssefoesmail:R51ngg1wNbB6T4xk@boycott.dpmudao.mongodb.net/")
   .then(() => {
     console.log("connected to DB");
   })
